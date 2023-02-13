c4b24d66f66f0c3198ea0416c9611705bbf00b5d
mathiaskerr <114953633+mathiaskerr@users.noreply.github.com>
2023-02-13 21:23:12 +0000

    x

--- homework.js
+++ homework.js
@@ -11,7 +11,7 @@
 //   const verdict = declareMurderer();
 //   console.log(verdict);
 
-//   miss scarlet will be returned as scenario is declared in global scope.
+//  1. miss scarlet will be returned as scenario is declared in global scope.
 
 // const murderer = 'Professor Plum';
 
@@ -27,5 +27,157 @@
 // const verdict = declareMurderer();
 // console.log(verdict);
 
-// guess = murderer will be professor plum because of const
-// correct = TypeError: Assignment to constant variable.
+// 2. guess = murderer will be professor plum because of const
+
+
+// let murderer = 'Professor Plum';
+
+// const declareMurderer = function() {
+//     let murderer = 'Mrs. Peacock';
+//     return `The murderer is ${murderer}.`;
+// }
+
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+// 3. guess = the murderer is pro plum because mrs peacock is assigned in the function
+
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+
+// 4. guess = 1st suspect 3 is colonel mustard as it is declared in the function and the 2nd one is mrs peacock
+
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Kitchen',
+//     weapon: 'Candle Stick'
+// };
+  
+// const changeWeapon = function(newWeapon) {
+//     scenario.weapon = newWeapon;
+// }
+  
+// const declareWeapon = function() {
+//     return `The weapon is the ${scenario.weapon}.`;
+// }
+  
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// 5.guess = weapon is revolver 
+
+// let murderer = 'Colonel Mustard';
+
+// const changeMurderer = function() {
+//     murderer = 'Mr. Green';
+
+// const plotTwist = function() {
+//     murderer = 'Mrs. White';
+// }
+
+// plotTwist();
+// }
+
+// const declareMurderer = function () {
+//     return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// 6. guess = murderer is mr green. did not see that bracket
+
+// let murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//     murderer = 'Mr. Green';
+
+// const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+
+// const unexpectedOutcome = function() {
+//     murderer = 'Miss Scarlet';
+// }
+
+// unexpectedOutcome();
+// }
+
+// plotTwist();
+// }
+
+// const declareMurderer = function() {
+//     return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// 7. guess = colonel mustard, I don't get this one
+
+// const scenario = {
+//     murderer: 'Mrs. Peacock',
+//     room: 'Conservatory',
+//     weapon: 'Lead Pipe'
+//   };
+  
+//   const changeScenario = function() {
+//     scenario.murderer = 'Mrs. Peacock';
+//     scenario.room = 'Dining Room';
+  
+//     const plotTwist = function(room) {
+//       if (scenario.room === room) {
+//         scenario.murderer = 'Colonel Mustard';
+//       }
+  
+//       const unexpectedOutcome = function(murderer) {
+//         if (scenario.murderer === murderer) {
+//           scenario.weapon = 'Candle Stick';
+//         }
+//       }
+  
+//       unexpectedOutcome('Colonel Mustard');
+//     }
+  
+//     plotTwist('Dining Room');
+//   }
+  
+//   const declareWeapon = function() {
+//     return `The weapon is ${scenario.weapon}.`
+//   }
+  
+//   changeScenario();
+//   const verdict = declareWeapon();
+//   console.log(verdict);
+
+  // 8. guess = weapon is candle stick
+
+  let murderer = 'Professor Plum';
+
+  if (murderer === 'Professor Plum') {
+    let murderer = 'Mrs. Peacock';
+  }
+  
+  const declareMurderer = function() {
+    return `The murderer is ${murderer}.`;
+  }
+  
+  const verdict = declareMurderer();
+  console.log(verdict);
+
+  // 9. guess = mrs peacock 
