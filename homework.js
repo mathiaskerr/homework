7fc234de1295d8eb645c7945f82b51a063c450ee
mathiaskerr <114953633+mathiaskerr@users.noreply.github.com>
2023-02-13 20:35:09 +0000

    h/w

---
+++ homework.js
@@ -0,0 +1,31 @@
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Library',
+//     weapon: 'Rope'
+//   };
+  
+//   const declareMurderer = function() {
+//     return `The murderer is ${scenario.murderer}.`;
+//   }
+  
+//   const verdict = declareMurderer();
+//   console.log(verdict);
+
+//   miss scarlet will be returned as scenario is declared in global scope.
+
+// const murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//     murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//     return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// guess = murderer will be professor plum because of const
+// correct = TypeError: Assignment to constant variable.
